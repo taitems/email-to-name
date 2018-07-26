215befa342084a680dee049f234662f81ae3c1bd
Tait Brown <taitbrown@gmail.com>
2018-07-26 22:12:45 +1000

    added generational and title handling

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
 var emailToName = require('./email-to-name.js');
-var testData = require('./test-data.js');
-
-console.log('testData.length', testData.length);
-console.log('typeof emailToName.process', typeof emailToName.process);
-
-var companyNames = ['acloudguru', 'acg', 'cloudguru'];
-
-for (var i = 0; i < testData.length; i++) {
-	var $test = testData[i];
-	var response = emailToName.process($test.input, companyNames, null);
-	if (response === $test.output) {
-		console.log('✅ Passed!')
-	} else {
-		console.log('❌ Error!');
-	}
-	console.log('Source:', $test.input);
-	console.log('Expected:', $test.output);
-	console.log('Received:', response);
-	console.log('------');
-
-}
+console.log(
+    emailToName.process('tait.brown.iii@gmail.com')
+);
